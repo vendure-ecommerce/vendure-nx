9c070111e14fdb8bafc2c37fdddf54616cb072e2
Draykee <officialdrayke@outlook.de>
2025-03-01 21:38:16 +0100

    chore: Added back BullMQJobQueuePlugin

--- libs/util-config/src/lib/vendure-config.ts
+++ libs/util-config/src/lib/vendure-config.ts
@@ -79,7 +79,16 @@ export const config: VendureConfig = {
           })
         : undefined,
     }),
-
+    BullMQJobQueuePlugin.init({
+      connection: {
+        host: process.env.REDIS_HOST ?? '127.0.0.1',
+        port: +process.env.NF_REDIS_PORT ?? 6379,
+        password: process.env.REDIS_PASSWORD ?? null,
+      },
+      queueOptions: {
+        defaultJobOptions: {},
+      },
+    }),
   ],
 };
 
